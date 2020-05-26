cf54a0439ca5ac4874e7b4f4ebc350984c40360e
winmord <winmord@yandex.ru>
2020-05-26 16:23:12 +0300

    Initial commit

---
+++ src/live_wallpapers/include/live_wallpapers/hello.hpp
@@ -0,0 +1,6 @@
+#include <string>
+
+std::string hello()
+{
+	return std::string("Hello");
+}
---
+++ src/live_wallpapers/src/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "include/live_wallpapers/hello.hpp"
+
+int main()
+{
+	std::cout << hello() << std::endl;
+	return 0;
+}
